62f0ebee9dadcb35e925d6407c6ed05a6c5932bc
s-sato <sayaka000521@gmail.com>
2025-07-09 16:00:22 +0900

    ヘッダーモック作成

---
+++ src/basics/NotificationBell.vue
@@ -0,0 +1,58 @@
+<script lang="ts">
+import { defineComponent } from 'vue'
+export default defineComponent({
+  name: 'NotificationBell',
+  data() {
+    return {
+      Notification: false, // 通知があるときtrue
+    }
+  },
+})
+</script>
+
+<template>
+  <div class="ellipse-wrapper">
+    <div class="ellipse-2"></div>
+    <div class="badge" v-if="Notification"></div>
+  </div>
+</template>
+
+<style scoped>
+.ellipse-wrapper {
+  position: fixed;
+  width: 30px;
+  height: 30px;
+  top: 27px;
+  right: 144px;
+  background-image: url('..\assets\images\doorbell.png');
+  background-size: 100% 100%;
+  cursor: pointer;
+  z-index: 995;
+}
+
+.ellipse-2 {
+  position: absolute;
+  width: 1px;
+  height: 1px;
+  top: 0;
+  right: 0;
+  background-color: var(--x1-primary-color);
+  border-radius: 50%;
+}
+
+.badge {
+  position: absolute;
+  top: 0;
+  right: 0;
+  width: 12px;
+  height: 12px;
+  background-color: red;
+  border-radius: 50%;
+}
+
+@media (max-width: 768px) {
+  .ellipse-wrapper {
+    right: 122px;
+  }
+}
+</style>
